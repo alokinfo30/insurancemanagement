709162adabd9fa36a84929df66de9df282de6800
Alok Srivastava <48906646+alokinfo30@users.noreply.github.com>
2026-09-25 03:20:08 +0530

    Configure static and media file settings
    Added static and media file configurations to settings.

--- insurancemanagement/settings.py
+++ insurancemanagement/settings.py
@@ -31,6 +31,13 @@ ALLOWED_HOSTS = [
     'testins.onrender.com',
 ]
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [STATIC_DIR]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -47,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
