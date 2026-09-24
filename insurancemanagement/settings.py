4121b81eba7ad2689b6825112c72c3e4d11ba8eb
Alok Srivastava <48906646+alokinfo30@users.noreply.github.com>
2026-09-25 03:10:19 +0530

    Set DEBUG to False and update ALLOWED_HOSTS

--- insurancemanagement/settings.py
+++ insurancemanagement/settings.py
@@ -25,10 +25,11 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'static')
 SECRET_KEY = 'ls@!_(edqp*xy76kvbsst$07at(v^li*2&ew!^$8o(@wa6@a+$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    'testins.onrender.com',
+]
 
 # Application definition
 
